c711e9b07ebb7ab7ca262bbbf394d753332f28d4
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2024-03-14 16:31:02 +0000

    javascript3章練習

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 let user_hand = prompt('ジャンケンの手をグー、チョキ、パーの中で選んでください');
+while ((user_hand !="グー") && (user_hand !="チョキ") && (user_hand !="パー") && (user_hand != null)){
+  alert("グー、チョキ、パーのいずれかを入力してください");
+  user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください');
+}
 
 
 let js_hand = getJShand();
-
-
 let judge = winLose(user_hand, js_hand);
 
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+}
+else{
+  alert("またチャレンジしてね")
+}
 
-alert('あなたの選んだ手は'+ user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
 
 
 function getJShand (){
   let js_hand_num = Math.floor( Math.random() *3 );
-  let hand_name;
-
+  let hand;
   if (js_hand_num == 0){
-    hand_name = 'グー'
+    hand = 'グー'
   }
   else if (js_hand_num == 1){
-    hand_name = 'チョキ'
+    hand = 'チョキ'
   }
   else if (js_hand_num == 2){
-    hand_name = 'パー'
+    hand = 'パー'
   }
-
-  return hand_name;
+  return hand;
 }
 
 function winLose( user,js ){
